b6664626ad15bb63a0ca24a4928389703cf2dc54
sushicat <alee@madisoncollege.edu>
2022-04-09 14:23:42 -0500

    Ran amplify push
    don't know why it generated a .ts file, but it did

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "basicauth22s714b73b3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
